836b536e8653ddfb1b393b0651c9b0a648c5eb42
Fred <f.detienne@gmail.com>
2020-07-13 06:37:29 +0200

    gravity.py simulates a sun-earth system. Fixed constants and cleaned up code

--- gravity.py
+++ gravity.py
@@ -1,10 +1,14 @@
 #!env python
 
-from Vector import *
-
 import pygame
-from pygame.locals import *
+from Vector import Vector2D
 
+# Physics constants
+EARTH_M = 5.9722*pow(10, 24) # earth mass in kg
+SUN_M = 1.989*pow(10, 30) # sun mass in kg
+INITIAL_EARTH_VELOCITY = 30300 # average earth orbital speed in m/s
+ONE_AU = 149597870700 # mean sun-earth distance: one Astronomical Unit in meters
+G = 6.67430*pow(10, -11) # gravitational constant
 
 #Colors
 GRAY = (127, 127, 127)
@@ -15,35 +19,55 @@ YELLOW = (255, 255, 0)
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 
+
+def F(m1, m2, r):
+    return G*m1*m2/pow(r,2)
+
+def A(f, m):
+    return f/m
+
 def draw(screen, vector, size=10, color=RED):
-    pygame.draw.rect(screen, color, ((vector.x-size/2, vector.y-size/2),(size, size)))
+    pygame.draw.rect(screen, color, ((vector.x-size/2, vector.y-size/2), (size, size)))
 
 
-res_x = 1024
-res_y = 768
-pygame.init()
-screen = pygame.display.set_mode((res_x, res_y), pygame.FULLSCREEN)
+def main() -> None:
+    res_x = 1024
+    res_y = 768
+    pygame.init()
+    # screen = pygame.display.set_mode((res_x, res_y), pygame.FULLSCREEN)
+    screen = pygame.display.set_mode((res_x, res_y))
 
-sun = Vector2D(res_x/2, res_y/2)
-position = Vector2D(res_x/2, res_y/4)
-velocity = Vector2D(50, 0)
-acceleration = Vector2D(0, 0)
-delta_t = .1
+    screen_center = Vector2D(res_x/2, res_y/2)
+    scale = res_y/(2.5*ONE_AU)
 
-running = True
-while running:
-    #update
-    acceleration = sun-position
-    acceleration.norm = 20
-    velocity = velocity + acceleration*delta_t
-    position = position + velocity*delta_t
+    sun = Vector2D(0, 0)
+    earth = Vector2D(0, ONE_AU)
+    velocity = Vector2D(INITIAL_EARTH_VELOCITY, 0)
+
+    acceleration = Vector2D(0, 0)
+    delta_t = 3.65*86400 # in s. The delta_time for each frame.
+
+    running = True
 
     screen.fill(BLACK)
-    draw(screen, position, color=BLUE)
-    draw(screen, sun, color=YELLOW)
-    pygame.display.update()
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            key = event.key
-            if key in (pygame.K_q, pygame.K_ESCAPE):
-                running = False
+    while running:
+        #update
+        force = F(EARTH_M, SUN_M, (earth-sun).norm)
+        accel_norm = A(force, EARTH_M)
+        acceleration = sun-earth
+        acceleration.norm = accel_norm
+        velocity = velocity + acceleration*delta_t
+        earth = earth + velocity*delta_t
+
+        # screen.fill(BLACK)
+        draw(screen, earth*scale+screen_center, size=3, color=BLUE)
+        draw(screen, sun*scale+screen_center, color=YELLOW)
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                key = event.key
+                if key in (pygame.K_q, pygame.K_ESCAPE):
+                    running = False
+
+if __name__ == "__main__":
+    main()
